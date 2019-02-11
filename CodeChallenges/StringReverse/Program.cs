2c8e4d90021d7cd8ac2b5903eaa7e0ed3ab2829d
Paddy McGlone <paddymcglone@gmail.com>
2019-02-11 19:38:12 +0000

    Adding first project within new solution.

---
+++ CodeChallenges/StringReverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string to reverse!");
+            var inputString = Console.ReadLine();
+            var myArray = inputString.ToCharArray();
+
+            for (int i = 0, j = inputString.Length-1; i < j; i++, j--)
+            {
+                myArray[i] = inputString[j];
+                myArray[j] = inputString[i];
+            }
+            Console.WriteLine($"Your string: {new string(myArray)}");
+        }
+    }
+}
