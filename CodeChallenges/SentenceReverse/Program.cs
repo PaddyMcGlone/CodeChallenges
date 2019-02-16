88f7c06c0c915b85566dd1057c0d1d426b68719d
Paddy McGlone <paddymcglone@gmail.com>
2019-02-16 16:07:21 +0000

    Adding the string reverse first attempt.

---
+++ CodeChallenges/SentenceReverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceReverse
+{
+    class Program
+    {
+        // Original sentence : Hello world from console
+        // New sentence: console from world Hello
+
+        static void Main(string[] args)
+        {
+            const string input = "Hello world from console";
+            var output = "";
+            
+            var myArray = input.Split(' ').ToArray().Reverse();
+
+            foreach (var word in myArray)
+                output += string.Format("{0} ", word);
+
+            Console.WriteLine(output);
+        }
+    }
+}
