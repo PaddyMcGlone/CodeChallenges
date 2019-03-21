68a1f53785e6c553bd4df30a2b1d0800e20f7989
Paddy McGlone <paddymcglone@gmail.com>
2019-03-21 18:24:32 +0000

    Adding the char occurences count.

---
+++ CodeChallenges/CountCharOccurences/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountCharOccurences
+{
+    class Program
+    {
+        // Count the number of times a char appears within a sentence.
+
+        static void Main(string[] args)
+        {
+            var input = "Hello world";
+
+            int total = 0;
+
+            foreach (var c in input)
+            {
+                if (c == 'o') total++;
+            }
+
+            Console.WriteLine("The char 'o' appears : {0} times", total);
+            Console.ReadKey();
+        }
+    }
+}
